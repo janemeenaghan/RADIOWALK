08df1e686697dcbe8726aebe328ca2379993aef7
janemeenaghan <106706615+janemeenaghan@users.noreply.github.com>
2022-06-24 23:26:52 -0700

    Setup SlidingUpPanel of two RelativeLayouts
    See description. Also removed some more extraneous blank lines.

--- app/src/main/java/com/example/janecapstoneproject/MainActivity.java
+++ app/src/main/java/com/example/janecapstoneproject/MainActivity.java
@@ -1,14 +1,11 @@
 package com.example.janecapstoneproject;
-
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
-
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }

2c71947353854a6b91cbda5d9688685d8d2c0f31
janemeenaghan <106706615+janemeenaghan@users.noreply.github.com>
2022-07-05 16:21:00 -0700

    Committing for our meeting today

--- app/src/main/java/com/example/janecapstoneproject/MainActivity.java
+++ app/src/main/java/com/example/janecapstoneproject/MainActivity.java
@@ -1,23 +1,64 @@
 package com.example.janecapstoneproject;
+
 import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import android.Manifest;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.common.internal.Constants;
+import com.google.android.gms.location.CurrentLocationRequest;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapView;
 import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.BitmapDescriptor;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.CancellationToken;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnTokenCanceledListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 
 public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
     private MapView mMapView;
+    public FloatingActionButton addStationButton;
+    public int REQUEST_CODE = 1001;
+    public static final int DEFAULT_ZOOM = 10;
 
-    private static final String MAPVIEW_BUNDLE_KEY = "MapViewBundleKey";
+    public static final String TAG = "MainActivity";
+    public static final String MAPVIEW_BUNDLE_KEY = "MapViewBundleKey";
 
+    FusedLocationProviderClient fusedLocationProviderClient;
+    Location location;
+    GoogleMap globalMap;
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
+
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
 
 
         Bundle mapViewBundle = null;
@@ -29,7 +70,51 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
 
         mMapView.getMapAsync(this);
 
+        //only visible when there is no nearby station - hide when not
+        addStationButton = findViewById(R.id.createButton);
+
+        addStationButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //final LatLng point = getCoordinates();
+                //showAlertDialogForPoint(point);
+                //showAlertDialogForPoint(myLocation);
+            }
+        });
+
+        setupMap();
     }
+
+    public void setupMap() {
+        if (mMapView != null) {
+            mMapView.getMapAsync(new OnMapReadyCallback() {
+                @RequiresApi(api = Build.VERSION_CODES.M)
+                @Override
+                public void onMapReady(GoogleMap map) {
+                    globalMap = map;
+                    centerOnMyLocation();
+                }
+            });
+        }
+
+    }
+
+
+    public void addStation(String title, String snippet) {
+
+        BitmapDescriptor defaultMarker = BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_RED);
+        // listingPosition is a LatLng point
+        // Create the marker on the fragment
+        if (location != null) {
+            Marker mapMarker = globalMap.addMarker(new MarkerOptions()
+                    .position(new LatLng(location.getLatitude(), location.getLongitude()))
+                    .title(title)
+                    .snippet(snippet)
+                    .icon(defaultMarker));
+        }
+        //TODO: fail
+    }
+
 
     @Override
     public void onSaveInstanceState(Bundle outState) {
@@ -64,7 +149,7 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
 
     @Override
     public void onMapReady(GoogleMap map) {
-        map.addMarker(new MarkerOptions().position(new LatLng(0, 0)).title("Marker"));
+        //map.addMarker(new MarkerOptions().position(new LatLng(0, 0)).title("Marker"));
     }
 
     @Override
@@ -85,4 +170,133 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         mMapView.onLowMemory();
     }
 
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    private void centerOnMyLocation() {
+        if (!checkForLocationPermission()) {
+            requestPermissions(new String[]{Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_CODE);
+            return;
+        }
+        else{
+            getDeviceLocationAndUpdateMap();
+
+        }
+    }
+
+
+    private void getDeviceLocationAndUpdateMap() {
+        CancellationToken whatDoesThisMean;
+        Log.d(TAG, "top");
+        /*
+         * Get the best and most recent location of the device, which may be null in rare
+         * cases when a location is not available.
+         */
+        try {
+            if (checkForLocationPermission()) {
+                 whatDoesThisMean = new CancellationToken() {
+                    @NonNull
+                    @Override
+                    public CancellationToken onCanceledRequested(@NonNull OnTokenCanceledListener onTokenCanceledListener) {
+                        return null;
+                    }
+
+                    @Override
+                    public boolean isCancellationRequested() {
+                        return false;
+                    }
+                };
+
+                Task<Location> locationResult = fusedLocationProviderClient.getLastLocation();
+
+                locationResult.addOnCompleteListener(this, new OnCompleteListener<Location>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Location> task) {
+                        globalMap.setMyLocationEnabled(true);
+                        if (task.isSuccessful()) {
+                            // Set the map's camera position to the current location of the device.
+                            location = task.getResult();
+                            if (location != null) {
+                                globalMap.setMyLocationEnabled(true);
+                                globalMap.getUiSettings().setMyLocationButtonEnabled(true);
+                                globalMap.moveCamera(CameraUpdateFactory.newLatLngZoom(
+                                        new LatLng(location.getLatitude(),
+                                                location.getLongitude()), DEFAULT_ZOOM));
+                                //Log.e("Exception: %s", e.getMessage(), e);
+                            }
+                            else{
+                                Log.d(TAG, "Current location is null. Using defaults.");
+                                Log.e(TAG, "Exception: %s", task.getException());
+                                //TODO: fail
+                            }
+                        } else {
+                            Log.d(TAG, "Current location is null. Using defaults.");
+                            Log.e(TAG, "Exception: %s", task.getException());
+                            //TODO: fail
+                        }
+                    }
+                });
+            }
+        } catch (SecurityException e)  {
+            Log.e("Exception: %s", e.getMessage(), e);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == REQUEST_CODE){
+            if (grantResults.length > 0){
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                    if (checkForLocationPermission()) {
+                        getDeviceLocationAndUpdateMap();
+                    }
+                }
+            }
+        }
+        //TODO: pop ups keep asking for permission continuously or send user to settings
+    }
+
+    public boolean checkForLocationPermission(){
+        return ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED;
+    }
+
+    private void showAlertDialogForPoint(final LatLng point) {
+        // inflate message_item.xml view
+        View  messageView = LayoutInflater.from(MainActivity.this).
+                inflate(R.layout.message_item, null);
+        // Create alert dialog builder
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
+        // set message_item.xml to AlertDialog builder
+        alertDialogBuilder.setView(messageView);
+
+        // Create alert dialog
+        final AlertDialog alertDialog = alertDialogBuilder.create();
+
+        // Configure dialog button (OK)
+        alertDialog.setButton(DialogInterface.BUTTON_POSITIVE, "OK",
+                new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        // Define color of marker icon
+                        BitmapDescriptor defaultMarker =
+                                BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_GREEN);
+                        // Extract content from alert dialog
+                        String title = ((EditText) alertDialog.findViewById(R.id.etTitle)).
+                                getText().toString();
+                        String snippet = ((EditText) alertDialog.findViewById(R.id.etSnippet)).
+                                getText().toString();
+                        // Creates and adds marker to the map
+                        addStation(title, snippet);
+                    }
+                });
+
+        // Configure dialog button (Cancel)
+        alertDialog.setButton(DialogInterface.BUTTON_NEGATIVE, "Cancel",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) { dialog.cancel(); }
+                });
+
+        // Display the dialog
+        alertDialog.show();
+    }
 }

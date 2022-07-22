4773315c39bf414f57512518d89be1e9501760d6
janemeenaghan <106706615+janemeenaghan@users.noreply.github.com>
2022-07-22 11:37:51 -0700

    Setup for FB Integration
    Manifest, gradle build, App class, etc. setup for Facebook SDK and ParseFacebookUtils integration.

--- app/src/main/java/com/example/janecapstoneproject/ParseApplication.java
+++ app/src/main/java/com/example/janecapstoneproject/ParseApplication.java
@@ -1,7 +1,12 @@
 package com.example.janecapstoneproject;
 import android.app.Application;
+import android.util.Log;
+
+import com.facebook.FacebookSdk;
+import com.facebook.appevents.AppEventsLogger;
 import com.parse.*;
 import com.parse.Parse;
+import com.parse.facebook.ParseFacebookUtils;
 import com.rey.material.app.ThemeManager;
 import okhttp3.*;
 public class ParseApplication extends Application {
@@ -9,12 +14,16 @@ public class ParseApplication extends Application {
     public void onCreate() {
         super.onCreate();
         ThemeManager.init(this, 2, 0, null);
-        ParseObject.registerSubclass(Station.class);
+        FacebookSdk.sdkInitialize(getApplicationContext());
+        AppEventsLogger.activateApp(this);
 
+        ParseObject.registerSubclass(Station.class);
         Parse.initialize(new Parse.Configuration.Builder(this)
                 .applicationId(getString(R.string.back4app_app_id))
                 .clientKey(getString(R.string.back4app_client_key))
                 .server(getString(R.string.back4app_server_url))
                 .build());
+        ParseInstallation.getCurrentInstallation().saveInBackground();
+        ParseFacebookUtils.initialize(this);
     }
 }

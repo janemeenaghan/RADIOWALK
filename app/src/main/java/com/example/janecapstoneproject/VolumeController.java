dd003c1cd81483bbb37182a817ecdfe15ab2e19c
janemeenaghan <106706615+janemeenaghan@users.noreply.github.com>
2022-07-23 00:33:17 -0700

    Cleaned up

--- app/src/main/java/com/example/janecapstoneproject/VolumeController.java
+++ app/src/main/java/com/example/janecapstoneproject/VolumeController.java
@@ -1,16 +1,13 @@
 package com.example.janecapstoneproject;
-
 import static android.content.Context.AUDIO_SERVICE;
 import android.content.Context;
 import android.media.AudioManager;
 import android.util.Log;
 
 import java.util.ArrayList;
-
 public class VolumeController {
     private AudioManager leftAm;
     private ArrayList<VolumeController.VolumeCallback> callbacks = new ArrayList<>();
-
     public VolumeController(Context context){
         leftAm = (AudioManager)context.getSystemService(AUDIO_SERVICE);
         for (VolumeController.VolumeCallback callback : callbacks){
@@ -18,54 +15,36 @@ public class VolumeController {
             callback.onVolumeChanged(leftAm.getStreamVolume(AudioManager.STREAM_MUSIC));
         }
     }
-
     public int getMaxVolume(){
         return leftAm.getStreamMaxVolume(AudioManager.STREAM_MUSIC);
     }
-
     public float getVolumeIncrement(){
         return (float)(leftAm.getStreamMaxVolume(AudioManager.STREAM_MUSIC))/15;
     }
-
     public void setVolume(int volume){
         leftAm.setStreamVolume(AudioManager.STREAM_MUSIC, volume, 0);
-        /*for (VolumeController.VolumeCallback callback : callbacks){
-            callback.onVolumeChanged(leftAm.getStreamVolume(AudioManager.STREAM_MUSIC));
-        }*/
     }
     public int getVolume(){
         return leftAm.getStreamVolume(AudioManager.STREAM_MUSIC);
     }
-
     public void raiseVolume(){
-        //leftAm.adjustVolume(AudioManager.ADJUST_RAISE,0);
         float newValue = ((float)leftAm.getStreamVolume(AudioManager.STREAM_MUSIC)) + getVolumeIncrement();
-
         leftAm.setStreamVolume(AudioManager.STREAM_MUSIC, ((int)(newValue)),0);
-        /*for (VolumeController.VolumeCallback callback : callbacks) {
-            callback.onVolumeChanged(leftAm.getStreamVolume(AudioManager.STREAM_MUSIC));
-        }*/
     }
     public void lowerVolume(){
-        //leftAm.adjustVolume(AudioManager.ADJUST_LOWER,0);
         float newValue = ((float)leftAm.getStreamVolume(AudioManager.STREAM_MUSIC)) - getVolumeIncrement();
         leftAm.setStreamVolume(AudioManager.STREAM_MUSIC, ((int)(newValue)),0);
-        /*for (VolumeController.VolumeCallback callback : callbacks) {
-            callback.onVolumeChanged(leftAm.getStreamVolume(AudioManager.STREAM_MUSIC));
-        }*/
     }
     public void registerCallback(VolumeController.VolumeCallback volumeCallback){
         if (!callbacks.contains(volumeCallback)){
             callbacks.add(volumeCallback);
         }
     }
-
     public void unRegisterCallback(VolumeController.VolumeCallback callback){
         if (callbacks.contains(callback)){
             callbacks.remove(callback);
         }
     }
-
     public interface VolumeCallback {
         void onVolumeChanged(int volume);
         void onMaxVolumeChanged(int maxVolume);

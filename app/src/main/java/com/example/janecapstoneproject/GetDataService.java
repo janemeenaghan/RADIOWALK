f568deac28ba56fe4b1c3e39e6200b73430685c3
janemeenaghan <106706615+janemeenaghan@users.noreply.github.com>
2022-07-23 11:42:04 -0700

    Changed search by tag to not go by votes as it overrides input tag inclusion

--- app/src/main/java/com/example/janecapstoneproject/GetDataService.java
+++ app/src/main/java/com/example/janecapstoneproject/GetDataService.java
@@ -9,6 +9,6 @@ public interface GetDataService {
     @GET("/json/stations?limit=20&order=votes&reverse=true&hidebroken=true")
     Call<List<StationInfo>> getAllStations();
 
-    @GET("/json/stations/bytag/{tag}?limit=20&order=votes&reverse=true&hidebroken=true")
+    @GET("/json/stations/bytag/{tag}?limit=20&hidebroken=true")
     Call<List<StationInfo>> getStationsByTag(@Path("tag") String tag);
 }

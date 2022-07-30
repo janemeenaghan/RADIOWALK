e22c0b9f50bba4686708f2374b2bad7e9302ebe0
janemeenaghan <106706615+janemeenaghan@users.noreply.github.com>
2022-07-29 18:08:03 -0700

    Updated GET Service for fetching places into public stations

--- app/src/main/java/com/example/janecapstoneproject/GetDataService.java
+++ app/src/main/java/com/example/janecapstoneproject/GetDataService.java
@@ -5,11 +5,16 @@ import java.util.List;
 import retrofit2.Call;
 import retrofit2.http.GET;
 import retrofit2.http.Path;
+import retrofit2.http.Query;
 
 public interface GetDataService {
-    @GET("/json/stations?limit=20&order=votes&reverse=true&hidebroken=true")
-    Call<List<StationInfo>> getAllStations();
+    @GET("/json/stations")
+    Call<List<StationInfo>> getAllStations(@Query ("limit") int limit, @Query("order") String votesString, @Query("reverse") boolean true1, @Query("hidebroken") boolean true2, @Query("offset") int page);
 
-    @GET("/json/stations/bytag/{tag}?limit=20&hidebroken=true")
-    Call<List<StationInfo>> getStationsByTag(@Path("tag") String tag);
+    @GET("/json/stations/bytag/{tag}")
+    Call<List<StationInfo>> getStationsByTag(@Path("tag") String tag, @Query ("limit") int limit, @Query("hidebroken") boolean true1, @Query("offset") int page);
+
+    @GET("json?location=37.44744,-122.16532&radius=500&key=AIzaSyBlTDb5XGjtYo647vgQ-RwRfSVdxyX4hVc")
+    Call<PlaceForStation> getAllPlacesInArea();
+
 }
